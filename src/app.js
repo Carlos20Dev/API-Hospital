d8ae10447d7bc5ec550daa62098a595201f32654
CarlosDev <159094741+Carlos20Dev@users.noreply.github.com>
2024-11-15 16:29:11 -0500

    Update app.js
    Agregando ruta para empleado

--- src/app.js
+++ src/app.js
@@ -12,9 +12,8 @@ app.set('port',config.PORT)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 
-app.get('/', (req, res) => {
-    res.send('Hola esta es mi api')
-})
+//Rutas
+app.use('/empleado',empleadoRoutes)
 app.use(empleadoRoutes)
 
 export default app
