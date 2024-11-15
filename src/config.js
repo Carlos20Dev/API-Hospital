31259903a8da741cbaf18ec0d34e8c9edd4d6576
CarlosDev <159094741+Carlos20Dev@users.noreply.github.com>
2024-11-15 16:38:36 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 export default {
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.DB_PORT || 3000,
     USER: process.env.DB_USER || '',
     PASS: process.env.DB_PASSWORD || '',
     SERVER: process.env.DB_SERVER || '',
