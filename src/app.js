650b51fdae09dd91a1af77f69094adb0290ac591
Carlos20Dev <carlos.tezen.1999@gmail.com>
2024-11-15 19:42:11 -0500

    ruta de la raiz

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ const app = express()
 
 app.set('port',config.PORT)
 
+// Ruta para la raíz
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la API del Hospital');
+});
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 
